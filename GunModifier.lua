04d7bb392fe87cd471c2d1e66dba9c1f27ff9478
WhySoSirius <amogusagus765@gmail.com>
2026-03-11 15:16:33 +0700

    Create GunModifier.lua

---
+++ GunModifier.lua
@@ -0,0 +1,168 @@
+--https://docs.sirius.menu/rayfield
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "gun modifier",
+   Icon = 0, -- Use 0 for no icon
+   LoadingTitle = "hi there bitch",
+   LoadingSubtitle = "made by ohdbloxguy232",
+   ShowText = "modifier", -- for mobile users to unhide rayfield, change if you'd like
+   Theme = "DarkBlue",
+
+   ToggleUIKeybind = "L",
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false,
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil,
+      FileName = "Big Hub"
+   },
+
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink",
+      RememberJoins = true
+   },
+
+   KeySystem = false,
+   KeySettings = {
+      Title = "contact owner to get key",
+      Subtitle = "i was here",
+      Note = "GET KEY FROM SCRIPT OWNER (find it yourself)",
+      FileName = "Idk",
+      SaveKey = true,
+      GrabKeyFromSite = false,
+      Key = {"IHaveAFaceLULZ"}
+   }
+})
+
+local Tab = Window:CreateTab("gun modifier", 4483362458) -- Title, Image
+
+local Button = Tab:CreateButton({
+   Name = "Instant Reload",
+   Callback = function()
+local garbage = getgc(true)
+
+
+
+for i, table in pairs(garbage) do
+
+if type(table) == "table" and rawget(table, "ReloadTime") then
+
+rawset(table, "ReloadTime", 0)
+
+end
+
+end
+      print("button clicked")
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "inf ammo kinda",
+   Callback = function()
+local garbage = getgc(true)
+
+
+
+for i, table in pairs(garbage) do
+
+if type(table) == "table" and rawget(table, "AmmoPerMag") then
+
+rawset(table, "AmmoPerMag", 82728)
+
+end
+
+end
+      print("button clicked")
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "no gun windup time",
+   Callback = function()
+local garbage = getgc(true)
+
+
+
+for i, table in pairs(garbage) do
+
+if type(table) == "table" and rawget(table, "WindUpTime") then
+
+rawset(table, "WindUpTime", 0)
+
+end
+
+end
+      print("button clicked")
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "no gun wind down time",
+   Callback = function()
+local garbage = getgc(true)
+
+
+
+for i, table in pairs(garbage) do
+
+if type(table) == "table" and rawget(table, "WindDownTime") then
+
+rawset(table, "WindDownTime", 0)
+
+end
+
+end
+      print("button clicked")
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "firerate",
+   Callback = function()
+local garbage = getgc(true)
+
+
+
+for i, table in pairs(garbage) do
+
+if type(table) == "table" and rawget(table, "FireRate") then
+
+rawset(table, "FireRate", 0)
+
+end
+
+end
+      print("button clicked")
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "no spread",
+   Callback = function()
+local garbage = getgc(true)
+
+
+
+for i, table in pairs(garbage) do
+
+if type(table) == "table" and rawget(table, "Spread") then
+
+rawset(table, "Spread", 0)
+
+end
+
+end
+      print("button clicked")
+   end,
+})
+
+local Button = Tab:CreateButton({
+   Name = "what do i add next",
+   Callback = function()
+      print("button clicked")
+   end,
+})
